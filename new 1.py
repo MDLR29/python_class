5dbc992c4fd6b638347c55d297f2f7a55ec3fcdb
MDLR29 <42417549+MDLR29@users.noreply.github.com>
2018-08-22 14:45:22 -0500

    Add files via upload

---
+++ new 1.py
@@ -0,0 +1,27 @@
+# ejercicio menu
+import os # importar libreria del sistema 
+os.sistema(cls)
+print("menu principal")
+print("1 suma numeros")
+print("2 suma numeros")
+print("3 suma numeros")
+print("4 suma numeros")
+print(".:::dijite su opcion: " )
+
+num1 = int(input("ingrece primer numero: " ))
+num2 = int(input("ingrece segundo numero: "))
+opc = int(input("digite la opcion: "))
+if opc == 1 :
+	resta = num + num2
+	print(" la suma es :",suma)
+elif opc == 2 :
+	resta = num1 - num2	
+	print("la resta es: ",resta)
+
+elif opc == 3 :
+	multiplicacion = num1 * num2
+	print(" la multiplicacion es: ",multiplicacion)
+
+elif opc == 4 :
+	div = num1 / num2
+	print("la division es :",div)
