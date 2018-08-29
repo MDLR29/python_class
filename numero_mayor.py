61bd6d93d6bda5a61c333d99444fd090d9835155
“MDLR29” <alejosalas12356@gmail.com>
2018-08-29 12:28:28 -0500

    numero_mayor

---
+++ numero_mayor.py
@@ -0,0 +1,11 @@
+
+print("ingrese primer numero: ")
+n1=input()
+print("ingrese segundo numero: ")
+n2=input()
+if n1>n2 :
+    print("el numero mayor es: ",n1)
+else  :
+ 
+    print("el numero mayor es: ",n2)
+    
