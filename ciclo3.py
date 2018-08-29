c09ec1882e239f05c2094a77e6d2165ec4660a94
“MDLR29” <alejosalas12356@gmail.com>
2018-08-29 14:17:36 -0500

    ciclo3

---
+++ ciclo3.py
@@ -0,0 +1,8 @@
+#numeros con ciclo while
+tabla = int(input("digite la tabla a mltiplicar:  "))
+rango = int(input("digite el rango a mltiplicar:  "))
+num=1
+while num<=rango:
+    print tabla,"*",num,"=",tabla*num 
+    num=num+1
+    
