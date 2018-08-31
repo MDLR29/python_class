4db65a8e71f6bf7e1c57fe2cc25e80bf47171899
“MDLR29” <alejosalas12356@gmail.com>
2018-08-30 19:35:01 -0500

    algoritmo_numeros

---
+++ algoritmo_numeros.py
@@ -0,0 +1,7 @@
+contador = 0
+i=1
+while contador <=100:
+    print("la suma de los numeros es: ",contador)  
+    contador=contador+i
+print("*******************************")
+print("fin del bucle while")
