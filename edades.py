f1ca4bbff648c2651c891d7a3c2d775d0d514884
“MDLR29” <alejosalas12356@gmail.com>
2018-09-05 13:39:41 -0500

    edades

---
+++ edades.py
@@ -0,0 +1,21 @@
+#edad1=int(input("edad 1 es: "))
+#edad2=int(input("edad 2 es: "))
+#edad3=int(input("edad 3 es: "))
+#edad4=int(input("edad 4 es: "))
+#edad5=int(input("edad 5 es: "))
+#edad6=int(input("edad 6 es: "))
+#edad7=int(input("edad 7 es: "))
+#edad8=int(input("edad 8 es: "))
+#edad9=int(input("edad 9 es: "))
+#edad10=int(input("edad 10 es: "))
+#edad=(edad1+edad2+edad3+edad4+edad5+edad6+edad7+edad8+edad9+edad10)/10
+#print(" el promedio es :",edad)
+x=1
+sumed=0
+while x<=10 :
+    edad=int(input("edad  es: "))
+    sumed = sumed + edad
+    x=x+1
+promedio=sumed/10      
+
+print(promedio)
