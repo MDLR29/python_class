b45987d1d25468a0bff6a15ea13ace498c8bc553
MDLR29 <42417549+MDLR29@users.noreply.github.com>
2018-10-03 14:36:16 -0500

    Add files via upload

---
+++ SEMAFORO.ino
@@ -0,0 +1,25 @@
+int motor=10;
+int ledrojo=2;
+int ledamarillo=4;
+int ledverde=7;
+void setup() { pinMode(ledrojo,OUTPUT); pinMode(ledamarillo,OUTPUT); pinMode(ledverde,OUTPUT); pinMode(motor,OUTPUT); 
+  // put your setup code here, to run once:
+
+}
+
+void loop() {digitalWrite(ledrojo,HIGH);
+delay(500); //tiempo de espera
+digitalWrite(ledrojo,LOW);
+delay(300);
+digitalWrite(ledamarillo,HIGH);
+delay(500); //tiempo de espera
+digitalWrite(ledamarillo,LOW);
+delay(300);
+digitalWrite(ledverde,HIGH);
+delay(500); //tiempo de espera
+digitalWrite(ledverde,LOW);
+delay(300);
+digitalWrite(motor,HIGH);
+  // put your main code here, to run repeatedly:
+
+}
