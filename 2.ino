24d3075dab52b40d147335deb690689b36ab185f
MDLR29 <42417549+MDLR29@users.noreply.github.com>
2018-10-03 14:35:55 -0500

    Add files via upload

---
+++ 2.ino
@@ -0,0 +1,29 @@
+char opcion='1';
+int ledrojo=13;
+void setup() { Serial.begin(9600);
+pinMode(ledrojo,OUTPUT);
+  // put your setup code here, to run once:
+
+}
+
+void loop() {if(Serial.available()>0);
+{
+  opcion=Serial.read();
+  if(opcion=='0')
+  {Serial.println("menu");
+  Serial.println("1.encender led rojo");
+  Serial.println("2.apagar led rojo");
+    }
+    if(opcion=='1')
+    {digitalWrite(ledrojo,HIGH);
+    }else{
+      if(opcion=='2')
+      {digitalWrite(ledrojo,LOW);
+    }else{
+      Serial.println("opcion incorrecta");
+    }
+    }
+}
+
+  // put your main code here, to run repeatedly:
+}
