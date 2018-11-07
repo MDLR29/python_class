ce164718c9fb2e298a473e836ab3bf094c9eb497
“MDLR29” <alejosalas12356@gmail.com>
2018-11-07 13:23:58 -0500

    clase

---
+++ claculadora_avanzada.py
@@ -0,0 +1,22 @@
+# funciones aritmeticas
+def menu():
+    print("menu")
+    print("1 suma")
+    print("2 resta")
+    print("3 multiplicacion")
+    print("4 division")
+
+def datos ():
+    n1 = int(input(" ingrese primer numero: "))
+    n2 = int(input(" ingrese segundo numero: "))
+
+datos()
+menu()
+opc = int(input(" digite su opcion: "))
+def opcion():
+    
+
+
+
+    
+    
